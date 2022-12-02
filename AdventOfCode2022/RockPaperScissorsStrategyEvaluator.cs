f4b4461ff3aab734ca9b1a5337d975b8a7e387ba
Markus Reinhardt <tuxxilla@gmail.com>
2022-12-02 14:13:51 +0100

    Complete day 2, exercise 1

---
+++ AdventOfCode2022/RockPaperScissorsStrategyEvaluator.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode2022
+{
+    public class RockPaperScissorsStrategyEvaluator
+    {
+        private IDataProvider _dataProvider;
+
+        public RockPaperScissorsStrategyEvaluator(IDataProvider dataProvider)
+        {
+            _dataProvider = dataProvider;
+        }
+
+        public int GetStrategyScore()
+        {
+            return AllGameScores().OrderDescending().Sum();
+        }
+
+        public IEnumerable<int> AllGameScores() 
+        {
+            foreach (var gameResultEntry in _dataProvider.LoadData())
+            {
+                var score = 0;
+                var hisResult = GetHisResult(gameResultEntry);
+                var myResult = GetMyResult(gameResultEntry);
+
+                var iWon = (myResult == Result.Rock && hisResult == Result.Scissors) 
+                    || (myResult == Result.Paper && hisResult == Result.Rock)
+                    || (myResult == Result.Scissors && hisResult == Result.Paper);
+                var itWasADraw = myResult == hisResult;
+
+                if (iWon)
+                {
+                    score += 6;
+                }
+                if (itWasADraw)
+                {
+                    score += 3;
+                }
+
+                score += (int) myResult;
+                yield return score;
+            }
+        }
+
+        private Result GetHisResult(string gameResultEntry)
+        {
+            var result = gameResultEntry.First();
+            if (result == 'A')
+            {
+                return Result.Rock;
+            }
+            if (result == 'B')
+            {
+                return Result.Paper;
+            }
+            return Result.Scissors;
+        }
+
+        private Result GetMyResult(string gameResultEntry)
+        {
+            var result = gameResultEntry.Last();
+            if (result == 'X')
+            {
+                return Result.Rock;
+            }
+            if (result == 'Y')
+            {
+                return Result.Paper;
+            }
+            return Result.Scissors;
+        }
+    }
+
+    enum Result
+    {
+        Rock = 1, Paper = 2, Scissors = 3
+    }
+}
---
+++ AdventOfCode2022Tests/RockPaperScissorsStrategyEvaluatorTest.cs
@@ -0,0 +1,48 @@
+﻿using AdventOfCode2022;
+using FluentAssertions;
+using NSubstitute;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace AdventOfCode2022Tests
+{
+    public class RockPaperScissorsStrategyEvaluatorTest
+    {
+        [Test]
+        public void EmptyList_ReturnsZeroScore()
+        {
+            var dataProvider = Substitute.For<IDataProvider>();
+            dataProvider.LoadData().Returns(new List<string>());
+            var sut = CreateCalorieCalculator(dataProvider);
+
+            sut.GetStrategyScore().Should().Be(0);
+        }
+
+        [Test]
+        public void ExampleStrategy_ReturnsCorrectScore()
+        {
+            var dataProvider = Substitute.For<IDataProvider>();
+            dataProvider.LoadData().Returns(new List<string>()
+            {
+                "A Y", "B X", "C Z"
+            });
+            var sut = CreateCalorieCalculator(dataProvider);
+
+            sut.GetStrategyScore().Should().Be(15);
+        }
+
+
+        [Test]
+        public void ExcerciseList_ReturnsCorrectScore()
+        {
+            var sut = CreateCalorieCalculator(new ResourceDataProvider("AdventOfCode2022Tests.Data.Day2Exercise1.txt"));
+
+            sut.GetStrategyScore().Should().Be(14827);
+        }
+
+        private RockPaperScissorsStrategyEvaluator CreateCalorieCalculator(IDataProvider dataProvider)
+        {
+            return new RockPaperScissorsStrategyEvaluator(dataProvider);
+        }
+    }
+}
